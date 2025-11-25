18233806d0f49d34edf3760fb329b3ab561e20b7
ruben-marques-pbb <ruben.marques@flutter.com>
2025-11-25 10:24:23 +0000

    changed entry point

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 
 export default defineConfig({
-  base: '/blip-labs/',
+
   plugins: [react()],
   resolve: {
     extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
